eb6bb4da9c5b0e1c6d5ac8bbde43f2b5a60409eb
box16 <inoinobox16@gmail.com>
2023-10-27 21:28:40 +0900

    ファイルアップロードしない形で実装

--- app.py
+++ app.py
@@ -6,39 +6,19 @@ from PIL import Image
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-
 model = load_model("leaf_disease_model.h5")
-
-# 画像のアップロード先の設定
-THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
-UPLOAD_FOLDER = os.path.join(THIS_FOLDER, "static/uploads")
 ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg"}
-app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 # 画像のファイル名が許可された拡張子かどうかをチェック
 def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
-
-# UPLOAD_FOLDERのクリーニング
-def delete_all_files_in_folder(folder_path):
-    if not os.path.exists(folder_path):
-        return
-    for filename in os.listdir(folder_path):
-        file_path = os.path.join(folder_path, filename)
-        if os.path.isfile(file_path):
-            os.remove(file_path)
 
 @app.route("/", methods=["GET", "POST"])
 def classify_leaf():
     if request.method == "POST":
         file = request.files["file"]
         if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
-            delete_all_files_in_folder(UPLOAD_FOLDER)
-            file.save(filepath)
-
-            img = Image.open(filepath).resize((128, 128))
+            img = Image.open(file).resize((128, 128))
             img = np.array(img)
             img = img / 255.0  # 画像データを正規化
             img = np.expand_dims(img, axis=0)  # バッチの次元を追加
@@ -49,11 +29,9 @@ def classify_leaf():
             else:
                 classification = "病気の葉"
             
-            return render_template("index.html", result=classification, image_file=filename)
+            return render_template("index.html", result=classification)
 
     return render_template("index.html", result=None)
 
 if __name__ == "__main__":
-    if not os.path.exists(UPLOAD_FOLDER):
-        os.makedirs(UPLOAD_FOLDER)
     app.run(debug=False)
