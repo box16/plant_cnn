54e159a15113f5e021a2ae287e1a3481a9dbcc4e
box16 <inoinobox16@gmail.com>
2023-12-25 16:58:13 +0900

    black適用

--- app.py
+++ app.py
@@ -9,16 +9,18 @@ app = Flask(__name__)
 model = load_model("leaf_disease_model.h5")
 ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg"}
 
+
 # 画像のファイル名が許可された拡張子かどうかをチェック
 def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route("/", methods=["GET", "POST"])
 def classify_leaf():
     if request.method == "POST":
         file = request.files["file"]
         if file and allowed_file(file.filename):
-            img = Image.open(file).convert('RGB').resize((128, 128))
+            img = Image.open(file).convert("RGB").resize((128, 128))
             img = np.array(img)
             img = img / 255.0  # 画像データを正規化
             img = np.expand_dims(img, axis=0)  # バッチの次元を追加
@@ -28,10 +30,11 @@ def classify_leaf():
                 classification = "健康な葉"
             else:
                 classification = "病気の葉"
-            
+
             return render_template("index.html", result=classification)
 
     return render_template("index.html", result=None)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
